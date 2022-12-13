0690e62b53248aca27a9c89898010f3040e79a5c
Denis <budarin@MacBook-Pro-Denis.local>
2022-12-13 20:59:45 +0300

    save

--- Program.cs
+++ Program.cs
@@ -1,8 +1,1 @@
-﻿Console.Write("\nВведите натуральное число N: ");
-int N = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("");
-
-for (int i = 1; i <= N; i++)
-{
-    Console.Write(+i * i * i + "\t");
-}
+﻿
